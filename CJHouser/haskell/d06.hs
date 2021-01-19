ac3cc8dc6f9f80eae692dbc6e34aab493497c747
Charles Joshua Houser <cjohouser@gmail.com>
2021-01-18 18:44:38 -0800

    d06: haskell part 1

---
+++ CJHouser/haskell/d06.hs
@@ -0,0 +1,15 @@
+import System.IO
+
+-- Counts the number of unique answers per group
+part1 :: String -> [Char] -> Int
+part1 [] acc = length acc                           -- Input finished
+part1 ('\n':'\n':xs) acc = part1 cs [] + length acc -- Group finished: empty acc
+part1 ('\n':xs) acc = part1 xs acc                  -- Skip whitespace
+part1 (x:xs) acc                                    -- Add unique characters to acc
+    | elem x acc = part1 xs acc
+    | otherwise  = part1 xs (x:acc)
+
+main = do
+    contents <- readFile "../inputs/d06"
+    let p1 = part1 contents []
+    print p1
