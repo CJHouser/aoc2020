e49f81b022db0b2319036d482c401bfc79fffa98
Charles Joshua Houser <cjohouser@gmail.com>
2021-01-18 19:01:28 -0800

    d06: oops

--- CJHouser/haskell/d06.hs
+++ CJHouser/haskell/d06.hs
@@ -3,7 +3,7 @@ import System.IO
 -- Counts the number of unique answers per group
 part1 :: String -> [Char] -> Int
 part1 [] acc = length acc                           -- Input finished
-part1 ('\n':'\n':xs) acc = part1 cs [] + length acc -- Group finished: empty acc
+part1 ('\n':'\n':xs) acc = part1 xs [] + length acc -- Group finished: empty acc
 part1 ('\n':xs) acc = part1 xs acc                  -- Skip whitespace
 part1 (x:xs) acc                                    -- Add unique characters to acc
     | elem x acc = part1 xs acc
